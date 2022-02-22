f1953ed637d0af6648818b7b124b5eb1f126d4ae
vuhoantran <vuhoantran@gmail.com>
2022-02-21 17:50:08 -0800

    Completed module

---
+++ stats/data.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import pandas as pd
+
+# Python File Management
+game_files = glob.glob(os.path.join(os.getcwd(),'games', '*.EVE'))
+
+# Sorting File Names
+list.sort(game_files)
+
+# Append Game Frames
+game_frames = []
+
+# Read CSV Files
+for game_file in game_files:
+    game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
+    game_frames.append(game_frame)
+
+# Concatenate DataFrames
+games = pd.concat([game_frames])
+
+# Clean Values
+games.loc[games['multi5'] == '??'] = ''
+
+# Extract Identifiers
+identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+
+# Forward Fill Identifiers
+identifiers = identifiers.fillna(method='ffill')
+
+# Rename Columns
+identifiers.columns = ['game_id', 'year']
+
+# Concatenate Identifier Columns
+games = pd.concat([games, identifiers], axis=1, sort=False)
+
+# Fill NaN Values
+games = games.fillna(' ')
+
+# Categorical Event Type
+games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'], categories=['info', 'start', 'play', 'com', 'sub', 'data'])
+
+# Print DataFrame
+games.head(5)
