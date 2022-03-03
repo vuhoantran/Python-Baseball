4b7ee7dedfadcd627e3af95c12f5e9993119fa7e
vuhoantran <vuhoantran@gmail.com>
2022-03-03 15:13:49 -0800

    Completed module

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from data import games
+
+# Select All Plays
+plays = games[games['type'] == 'play']
+
+# Select All Strike Outs
+strike_outs = plays[plays['event'].str.contains('K')]
+
+# Group by Year and Game
+strike_outs = strike_outs.groupby(['game_id', 'year']).size()
+
+# Reset Index
+strike_outs = strike_outs.reset_index(name='strike_outs')
+
+# Apply an Operation to Multiple Columns
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+# Change Plot Formatting
+strike_outs.plot(x='year', y='strike_outs', label='Strike Outs', kind='scatter').legend()
+
+plt.show()
+
+print(strike_outs.head())
